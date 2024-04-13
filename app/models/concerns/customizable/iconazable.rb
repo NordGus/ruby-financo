829f11d429d52e814595b3a49c9693d786f90a91
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-04-13 20:14:46 +0200

    adjusting rubocop styling

--- app/models/concerns/customizable/iconazable.rb
+++ app/models/concerns/customizable/iconazable.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
 module Customizable
-  # rubocop:disable Metrics/ModuleLength
   module Iconazable
     extend ActiveSupport::Concern
 
     # TODO(#1): search and list icon theme
     ICONS = {
-      bank: 'icon-bank'
+      bank: "icon-bank"
     }.freeze
 
     included do
       validates :icon, presence: true, inclusion: { in: ICONS.keys.map(&:to_s) }
     end
   end
-  # rubocop:enable Metrics/ModuleLength
 end
