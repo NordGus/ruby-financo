e896800a23ed350e8b89680c53d213677767f183
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-04-21 21:42:21 +0200

    creating a simple seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,36 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# rubocop:disable Metrics/BlockLength
+ActiveRecord::Base.transaction do
+  personal_account = Account.create_with(kind: Account::KINDS[:capital][:normal],
+                                         currency: "EUR",
+                                         capital: 0,
+                                         color: "blue.500")
+                            .find_or_create_by!(name: "Personal")
+  income_account = Account.create_with(kind: Account::KINDS[:external][:income],
+                                       currency: "EUR",
+                                       capital: 0,
+                                       color: "green.500")
+                          .find_or_create_by(name: "Income")
+  expense_account = Account.create_with(kind: Account::KINDS[:external][:expense],
+                                        currency: "EUR",
+                                        capital: 0,
+                                        color: "red.500")
+                           .find_or_create_by!(name: "Expense")
+
+  if (personal_account.credits.count + personal_account.debits.count) != 2
+    Transaction.create!(source: income_account,
+                        target: personal_account,
+                        source_amount: 250_000,
+                        target_amount: 250_000,
+                        issued_at: Date.today,
+                        executed_at: Date.today)
+    Transaction.create!(source: personal_account,
+                        target: expense_account,
+                        source_amount: 50_000,
+                        target_amount: 50_000,
+                        issued_at: Date.today,
+                        executed_at: Date.today)
+  end
+end
+# rubocop:enable Metrics/BlockLength
