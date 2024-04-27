5fb67609fa283c38689a4d58a576b40a653cb490
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-04-27 21:29:39 +0200

    implementing total summary

--- app/controllers/accounts_and_goals/summary_controller.rb
+++ app/controllers/accounts_and_goals/summary_controller.rb
@@ -6,44 +6,65 @@ module AccountsAndGoals
     before_action :calculate_credits, only: %i[capital debt]
     before_action :calculate_debits, only: %i[capital debt]
 
+    before_action :calculate_capital_balance, only: :total
+    before_action :calculate_debt_balance, only: :total
+
     def capital
-      @summary = calculate_summary
+      @summary = calculate_summary(@debits, @credits)
 
       render json: { summary: @summary }
     end
 
     def debt
-      @summary = calculate_summary
+      @summary = calculate_summary(@debits, @credits)
 
       render json: { summary: @summary }
     end
 
     def total
-      render json: { summary: nil }
+      @summary = @capital_balance.merge(@debt_balance) { |_key, capital, debt| capital + debt }
+
+      render json: { summary: @summary }
     end
 
     private
 
     def set_account_kinds
-      @account_kinds = Account::KINDS.slice(params[:action].to_sym, :system).values.map(&:values).flatten
+      @account_kinds = Account::KINDS[params[:action].to_sym].values
     end
 
     def calculate_credits
-      @credits = ActiveRecord::Base.connection
-                                   .execute(credits_sql_query(@account_kinds))
-                                   .to_a
-                                   .map(&:deep_symbolize_keys)
-                                   .group_by { |credit| credit[:currency] }
-                                   .transform_values { |currency| currency.sum { |credit| credit[:amount] } }
+      @credits = retrieve_and_map_data(credits_sql_query(@account_kinds))
     end
 
     def calculate_debits
-      @debits = ActiveRecord::Base.connection
-                                  .execute(debits_sql_query(@account_kinds))
-                                  .to_a
-                                  .map(&:deep_symbolize_keys)
-                                  .group_by { |debit| debit[:currency] }
-                                  .transform_values { |currency| currency.sum { |debit| debit[:amount] } }
+      @debits = retrieve_and_map_data(debits_sql_query(@account_kinds))
+    end
+
+    def calculate_capital_balance
+      kinds = Account::KINDS[:capital].values
+
+      @capital_balance = calculate_summary(
+        retrieve_and_map_data(debits_sql_query(kinds)),
+        retrieve_and_map_data(credits_sql_query(kinds))
+      )
+    end
+
+    def calculate_debt_balance
+      kinds = Account::KINDS[:debt].values
+
+      @debt_balance = calculate_summary(
+        retrieve_and_map_data(debits_sql_query(kinds)),
+        retrieve_and_map_data(credits_sql_query(kinds))
+      )
+    end
+
+    def retrieve_and_map_data(query)
+      ActiveRecord::Base.connection.execute(query)
+                        .to_a
+                        .map(&:deep_symbolize_keys)
+                        .group_by { |entry| entry[:currency] }
+                        .transform_values { |currency| currency.sum { |entry| entry[:amount] } }
     end
 
     def debits_sql_query(kinds)
@@ -69,8 +90,8 @@ module AccountsAndGoals
       SQL
     end
 
-    def calculate_summary
-      @debits.merge(@credits) { |_key, debit, credit| debit + credit }
+    def calculate_summary(debits, credits)
+      debits.merge(credits) { |_key, debit, credit| debit + credit }
     end
   end
 end
