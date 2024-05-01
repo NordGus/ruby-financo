c9bca0024660f9b0720090d50a993644b3b3c23a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-01 20:04:28 +0200

    designing capital account form

--- app/forms/accounts_and_goals/accounts/capital_form.rb
+++ app/forms/accounts_and_goals/accounts/capital_form.rb
@@ -3,6 +3,10 @@
 module AccountsAndGoals
   module Accounts
     class CapitalForm < AccountForm
+      attribute :amount, :integer
+      attribute :at, :date
+
+      validates :at, comparison: { less_than_or_equal_to: Time.zone.today }, if: -> { at.present? }
     end
   end
 end
