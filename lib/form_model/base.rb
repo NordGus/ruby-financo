82f16a4b3b22045f4be90ce8b29aceee2554085c
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-01 21:13:57 +0200

    adding callbacks to FormModel::Base

--- lib/form_model/base.rb
+++ lib/form_model/base.rb
@@ -3,7 +3,16 @@
 module FormModel
   class Base
     include ActiveModel::Model
+    extend ActiveModel::Callbacks
     include ActiveModel::Attributes
+
+    define_model_callbacks :initialize, only: :after
+
+    def initialize(*)
+      run_callbacks :initialize do
+        super(*)
+      end
+    end
 
     def persisted?
       raise NotImplementedError
