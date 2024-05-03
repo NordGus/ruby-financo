bc030fcec1ef2525ef9521d5eff7c3a69c84ba8a
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-03 23:24:00 +0200

    building from https://github.com/rbgrouleff/rails/blob/add_permissions_policy_header/actionpack/lib/action_dispatch/http/permissions_policy.rb patch to permissions_policy.rb

--- lib/monkey_patches/permissions_policy/middleware/call_patch.rb
+++ lib/monkey_patches/permissions_policy/middleware/call_patch.rb
@@ -62,7 +62,14 @@ module MonkeyPatches
         end
 
         module InstanceMethods
-          PERMISSIONS_POLICY = "permissions-policy"
+          CONTENT_TYPE       = "Content-Type"
+          # The Feature-Policy header has been renamed to Permissions-Policy.
+          # Since the Permissions-Policy isn't yet supported by all browsers,
+          # this middleware inserts both headers. For now `POLICY` still refers
+          # to the old header name. This will change in the future, when
+          # Permissions-Policy is supported by all browsers.
+          POLICY             = "Feature-Policy"
+          PERMISSIONS_POLICY = "Permissions-Policy"
 
           def call(env)
             _, headers, = response = @app.call(env)
@@ -73,12 +80,179 @@ module MonkeyPatches
             request = ActionDispatch::Request.new(env)
 
             if (policy = request.permissions_policy)
-              headers[PERMISSIONS_POLICY] = policy.build(request.controller_instance)
+              headers[PERMISSIONS_POLICY] = PermissionsPolicyHeader.build(policy, request.controller_instance)
+              headers[POLICY] = FeaturePolicyHeader.build(policy, request.controller_instance)
             end
 
-            headers.delete(PERMISSIONS_POLICY) if policy_empty?(policy)
+            if policy_empty?(policy)
+              headers.delete(PERMISSIONS_POLICY)
+              headers.delete(POLICY)
+            end
 
             response
+          end
+          class << self
+            private
+
+            def html_response?(headers)
+              return unless (content_type = headers[CONTENT_TYPE])
+
+              content_type.include?("html")
+            end
+
+            def policy_present?(headers)
+              headers[PERMISSIONS_POLICY] || headers[POLICY]
+            end
+
+            def policy_empty?(policy)
+              policy&.directives&.empty?
+            end
+          end
+
+          module PermissionsPolicyHeader
+            MAPPINGS = {
+              self: "self",
+              none: "",
+              all: "*"
+            }.freeze
+
+            module_function
+
+            def build(policy, context = nil)
+              build_directives(policy, context).compact.join(", ")
+            end
+
+            def build_directives(policy, context)
+              policy.directives.map do |directive, s|
+                sources = apply_mappings(s)
+                if sources.is_a?(Array)
+                  allow_list = build_allow_list(sources, context)
+                  if allow_list.include?(MAPPINGS[:none])
+                    "#{directive}=()"
+                  elsif allow_list.include?(MAPPINGS[:all])
+                    "#{directive}=#{MAPPINGS[:all]}"
+                  else
+                    "#{directive}=(#{allow_list.join(' ')})"
+                  end
+                elsif sources
+                  directive
+                end
+              end
+            end
+
+            def build_allow_list(sources, context)
+              sources.map { |source| resolve_source(source, context) }
+            end
+
+            def resolve_source(source, context)
+              case source
+              when String
+                if MAPPINGS.value?(source)
+                  source
+                else
+                  "\"#{source}\""
+                end
+              when Symbol
+                source.to_s
+              when Proc
+                raise "Missing context for the dynamic permissions policy source: #{source.inspect}" if context.nil?
+
+                context.instance_exec(&source)
+
+              else
+                raise "Unexpected permissions policy source: #{source.inspect}"
+              end
+            end
+
+            def apply_mappings(sources)
+              sources.map do |source|
+                case source
+                when Symbol
+                  apply_mapping(source)
+                when String, Proc
+                  source
+                else
+                  raise ArgumentError, "Invalid HTTP permissions policy source: #{source.inspect}"
+                end
+              end
+            end
+
+            def apply_mapping(source)
+              MAPPINGS.fetch(source) do
+                raise ArgumentError, "Unknown HTTP permissions policy source mapping: #{source.inspect}"
+              end
+            end
+          end
+
+          module FeaturePolicyHeader
+            MAPPINGS = {
+              self: "'self'",
+              none: "'none'",
+              all: "*"
+            }.freeze
+
+            module_function
+
+            def build(policy, context = nil)
+              build_directives(policy, context).compact.join("; ")
+            end
+
+            def build_directives(policy, context)
+              policy.directives.map do |directive, s|
+                sources = apply_mappings(s)
+                if sources.is_a?(Array)
+                  allow_list = build_allow_list(sources, context)
+                  if allow_list.include?(MAPPINGS[:none])
+                    "#{directive} #{MAPPINGS[:none]}"
+                  elsif allow_list.include?(MAPPINGS[:all])
+                    "#{directive} #{MAPPINGS[:all]}"
+                  else
+                    "#{directive} #{allow_list.join(' ')}"
+                  end
+                elsif sources
+                  directive
+                end
+              end
+            end
+
+            def build_allow_list(sources, context)
+              sources.map { |source| resolve_source(source, context) }
+            end
+
+            def resolve_source(source, context)
+              case source
+              when String
+                source
+              when Symbol
+                source.to_s
+              when Proc
+                raise "Missing context for the dynamic permissions policy source: #{source.inspect}" if context.nil?
+
+                context.instance_exec(&source)
+
+              else
+                raise "Unexpected permissions policy source: #{source.inspect}"
+              end
+            end
+
+            def apply_mappings(sources)
+              sources.map do |source|
+                case source
+                when Symbol
+                  apply_mapping(source)
+                when String, Proc
+                  source
+                else
+                  raise ArgumentError, "Invalid HTTP permissions policy source: #{source.inspect}"
+                end
+              end
+            end
+
+            def apply_mapping(source)
+              MAPPINGS.fetch(source) do
+                raise ArgumentError, "Unknown HTTP permissions policy source mapping: #{source.inspect}"
+              end
+            end
           end
         end
       end
