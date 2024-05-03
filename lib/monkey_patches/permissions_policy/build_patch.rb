3f0541f919ddeb8899878dd020af31116d4d07df
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-03 23:47:45 +0200

    fixing build_patch

--- lib/monkey_patches/permissions_policy/build_patch.rb
+++ lib/monkey_patches/permissions_policy/build_patch.rb
@@ -4,70 +4,68 @@ require "action_dispatch"
 
 module MonkeyPatches
   module PermissionsPolicy
-    module Middleware
-      module BuildPatch
-        class << self
-          EXPIRATION_DATE = Date.new(2024, 8, 15)
+    module BuildPatch
+      class << self
+        EXPIRATION_DATE = Date.new(2024, 8, 15)
 
-          def apply_patch
-            if Date.today > EXPIRATION_DATE
-              puts "WARNING: Please re-evaluate whether or not the "\
-                     "ActionDispatch::PermissionsPolicy build"\
-                     "patch present in #{__FILE__} is still necessary."
-            end
-
-            const = find_const
-            mtd = find_method(const)
-
-            # make sure the class we want to patch exists;
-            # make sure the #build method exists and accepts exactly
-            # one argument
-            unless const && mtd && mtd.arity == 1
-              raise "Could not find class or method when patching "\
-                      "ActionDispatch::PermissionsPolicy's build method."\
-                      "Please investigate."
-            end
-
-            # if rails has been upgraded, make sure this patch is still
-            # necessary
-            unless rails_version_ok?
-              puts "WARNING: It looks like Rails has been upgraded since "\
-                     "ActionDispatch::PermissionsPolicy's build method"\
-                     "was monkeypatched in #{__FILE__}. Please re-evaluate the patch."
-            end
-
-            # actually apply the patch
-            const.prepend(InstanceMethods)
+        def apply_patch
+          if Date.today > EXPIRATION_DATE
+            puts "WARNING: Please re-evaluate whether or not the "\
+                   "ActionDispatch::PermissionsPolicy build"\
+                   "patch present in #{__FILE__} is still necessary."
           end
 
-          private
+          const = find_const
+          mtd = find_method(const)
 
-          def find_const
-            Kernel.const_get("ActionDispatch::PermissionsPolicy")
-          rescue NameError
-            # return nil if the constant doesn't exist
+          # make sure the class we want to patch exists;
+          # make sure the #build method exists and accepts exactly
+          # one nillable argument
+          unless const && mtd && mtd.arity == -1
+            raise "Could not find class or method when patching "\
+                    "ActionDispatch::PermissionsPolicy's build method."\
+                    "Please investigate."
           end
 
-          def find_method(const)
-            return unless const
-
-            const.instance_method(:build)
-          rescue NameError
-            # return nil if the constant doesn't exist
+          # if rails has been upgraded, make sure this patch is still
+          # necessary
+          unless rails_version_ok?
+            puts "WARNING: It looks like Rails has been upgraded since "\
+                   "ActionDispatch::PermissionsPolicy's build method"\
+                   "was monkeypatched in #{__FILE__}. Please re-evaluate the patch."
           end
 
-          def rails_version_ok?
-            Rails::VERSION::MAJOR == 7 && Rails::VERSION::MINOR == 1
-          end
+          # actually apply the patch
+          const.prepend(InstanceMethods)
         end
 
-        module InstanceMethods
-          def build(context = nil, builder: nil)
-            if builder
-              builder.build(self, context)
-            else
-              super
-            end
+        private
+
+        def find_const
+          Kernel.const_get("ActionDispatch::PermissionsPolicy")
+        rescue NameError
+          # return nil if the constant doesn't exist
+        end
+
+        def find_method(const)
+          return unless const
+
+          const.instance_method(:build)
+        rescue NameError
+          # return nil if the constant doesn't exist
+        end
+
+        def rails_version_ok?
+          Rails::VERSION::MAJOR == 7 && Rails::VERSION::MINOR == 1
+        end
+      end
+
+      module InstanceMethods
+        def build(context = nil, builder: nil)
+          if builder
+            builder.build(self, context)
+          else
+            super(context)
           end
         end
       end
@@ -75,4 +73,4 @@ module MonkeyPatches
   end
 end
 
-MonkeyPatches::PermissionsPolicy::Middleware::BuildPatch.apply_patch
+MonkeyPatches::PermissionsPolicy::BuildPatch.apply_patch
