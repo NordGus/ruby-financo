482528cd944689619bddfb8fd04fd25b92d9a0a4
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-03 23:47:15 +0200

    bandaging my foot

--- lib/monkey_patches/permissions_policy/middleware/call_patch.rb
+++ lib/monkey_patches/permissions_policy/middleware/call_patch.rb
@@ -92,7 +92,6 @@ module MonkeyPatches
             response
           end
 
-          # rubocop:disable Metrics/ClassLength
           class << self
             private
 
@@ -109,83 +108,82 @@ module MonkeyPatches
             def policy_empty?(policy)
               policy&.directives&.empty?
             end
+          end
+        end
 
-            module PermissionsPolicyHeader
-              MAPPINGS = {
-                self: "self",
-                none: "",
-                all: "*"
-              }.freeze
+        module PermissionsPolicyHeader
+          MAPPINGS = {
+            self: "self",
+            none: "",
+            all: "*"
+          }.freeze
 
-              module_function
+          module_function
 
-              def build(policy, context = nil)
-                build_directives(policy, context).compact.join(", ")
-              end
+          def build(policy, context = nil)
+            build_directives(policy, context).compact.join(", ")
+          end
 
-              def build_directives(policy, context)
-                policy.directives.map do |directive, s|
-                  sources = apply_mappings(s)
-                  if sources.is_a?(Array)
-                    allow_list = build_allow_list(sources, context)
-                    if allow_list.include?(MAPPINGS[:none])
-                      "#{directive}=()"
-                    elsif allow_list.include?(MAPPINGS[:all])
-                      "#{directive}=#{MAPPINGS[:all]}"
-                    else
-                      "#{directive}=(#{allow_list.join(' ')})"
-                    end
-                  elsif sources
-                    directive
-                  end
-                end
-              end
-
-              def build_allow_list(sources, context)
-                sources.map { |source| resolve_source(source, context) }
-              end
-
-              def resolve_source(source, context)
-                case source
-                when String
-                  if MAPPINGS.value?(source)
-                    source
-                  else
-                    "\"#{source}\""
-                  end
-                when Symbol
-                  source.to_s
-                when Proc
-                  raise "Missing context for the dynamic permissions policy source: #{source.inspect}" if context.nil?
-
-                  context.instance_exec(&source)
-
+          def build_directives(policy, context)
+            policy.directives.map do |directive, s|
+              sources = apply_mappings(s)
+              if sources.is_a?(Array)
+                allow_list = build_allow_list(sources, context)
+                if allow_list.include?(MAPPINGS[:none])
+                  "#{directive}=()"
+                elsif allow_list.include?(MAPPINGS[:all])
+                  "#{directive}=#{MAPPINGS[:all]}"
                 else
-                  raise "Unexpected permissions policy source: #{source.inspect}"
+                  "#{directive}=(#{allow_list.join(' ')})"
                 end
-              end
-
-              def apply_mappings(sources)
-                sources.map do |source|
-                  case source
-                  when Symbol
-                    apply_mapping(source)
-                  when String, Proc
-                    source
-                  else
-                    raise ArgumentError, "Invalid HTTP permissions policy source: #{source.inspect}"
-                  end
-                end
-              end
-
-              def apply_mapping(source)
-                MAPPINGS.fetch(source) do
-                  raise ArgumentError, "Unknown HTTP permissions policy source mapping: #{source.inspect}"
-                end
+              elsif sources
+                directive
               end
             end
           end
-          # rubocop:enable Metrics/ClassLength
+
+          def build_allow_list(sources, context)
+            sources.map { |source| resolve_source(source, context) }
+          end
+
+          def resolve_source(source, context)
+            case source
+            when String
+              if MAPPINGS.value?(source)
+                source
+              else
+                "\"#{source}\""
+              end
+            when Symbol
+              source.to_s
+            when Proc
+              raise "Missing context for the dynamic permissions policy source: #{source.inspect}" if context.nil?
+
+              context.instance_exec(&source)
+
+            else
+              raise "Unexpected permissions policy source: #{source.inspect}"
+            end
+          end
+
+          def apply_mappings(sources)
+            sources.map do |source|
+              case source
+              when Symbol
+                apply_mapping(source)
+              when String, Proc
+                source
+              else
+                raise ArgumentError, "Invalid HTTP permissions policy source: #{source.inspect}"
+              end
+            end
+          end
+
+          def apply_mapping(source)
+            MAPPINGS.fetch(source) do
+              raise ArgumentError, "Unknown HTTP permissions policy source mapping: #{source.inspect}"
+            end
+          end
         end
       end
     end
