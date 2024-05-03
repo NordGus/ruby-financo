cd105eeab1b559b09b802a1ef7d4fc182ca61cc5
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-03 23:49:14 +0200

    adding patches to permissions_policy.rb

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
+require_relative "../../lib/monkey_patches/permissions_policy/middleware/call_patch"
+require_relative "../../lib/monkey_patches/permissions_policy/build_patch"
+
 # Be sure to restart your server when you modify this file.
 
 # Define an application-wide HTTP permissions policy. For further
 # information see: https://developers.google.com/web/updates/2018/06/feature-policy
 
-# Rails.application.config.permissions_policy do |policy|
-#   policy.camera      :none
-#   policy.gyroscope   :none
-#   policy.microphone  :none
-#   policy.usb         :none
-#   policy.fullscreen  :self
-#   policy.payment     :self, "https://secure.example.com"
-# end
+Rails.application.config.permissions_policy do |policy|
+  policy.camera      :none
+  policy.gyroscope   :none
+  policy.microphone  :none
+  policy.usb         :none
+  policy.fullscreen  :self
+  policy.payment     :self, "https://secure.example.com"
+  policy.vr          :none
+end
