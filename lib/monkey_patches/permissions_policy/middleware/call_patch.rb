25f52e8a1d0b2b03414d1bfb9d82f0c3f6fdb15b
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-03 23:39:47 +0200

    removing FeaturePolicyHeader and left the old one

--- lib/monkey_patches/permissions_policy/middleware/call_patch.rb
+++ lib/monkey_patches/permissions_policy/middleware/call_patch.rb
@@ -81,7 +81,7 @@ module MonkeyPatches
 
             if (policy = request.permissions_policy)
               headers[PERMISSIONS_POLICY] = policy.build(request.controller_instance, builder: PermissionsPolicyHeader)
-              headers[FEATURE_POLICY] = policy.build(request.controller_instance, builder: FeaturePolicyHeader)
+              headers[FEATURE_POLICY] = policy.build(request.controller_instance)
             end
 
             if policy_empty?(policy)
@@ -153,77 +153,6 @@ module MonkeyPatches
                   else
                     "\"#{source}\""
                   end
-                when Symbol
-                  source.to_s
-                when Proc
-                  raise "Missing context for the dynamic permissions policy source: #{source.inspect}" if context.nil?
-
-                  context.instance_exec(&source)
-
-                else
-                  raise "Unexpected permissions policy source: #{source.inspect}"
-                end
-              end
-
-              def apply_mappings(sources)
-                sources.map do |source|
-                  case source
-                  when Symbol
-                    apply_mapping(source)
-                  when String, Proc
-                    source
-                  else
-                    raise ArgumentError, "Invalid HTTP permissions policy source: #{source.inspect}"
-                  end
-                end
-              end
-
-              def apply_mapping(source)
-                MAPPINGS.fetch(source) do
-                  raise ArgumentError, "Unknown HTTP permissions policy source mapping: #{source.inspect}"
-                end
-              end
-            end
-
-            module FeaturePolicyHeader
-              MAPPINGS = {
-                self: "'self'",
-                none: "'none'",
-                all: "*"
-              }.freeze
-
-              module_function
-
-              def build(policy, context = nil)
-                build_directives(policy, context).compact.join("; ")
-              end
-
-              def build_directives(policy, context)
-                policy.directives.map do |directive, s|
-                  sources = apply_mappings(s)
-                  if sources.is_a?(Array)
-                    allow_list = build_allow_list(sources, context)
-                    if allow_list.include?(MAPPINGS[:none])
-                      "#{directive} #{MAPPINGS[:none]}"
-                    elsif allow_list.include?(MAPPINGS[:all])
-                      "#{directive} #{MAPPINGS[:all]}"
-                    else
-                      "#{directive} #{allow_list.join(' ')}"
-                    end
-                  elsif sources
-                    directive
-                  end
-                end
-              end
-
-              def build_allow_list(sources, context)
-                sources.map { |source| resolve_source(source, context) }
-              end
-
-              def resolve_source(source, context)
-                case source
-                when String
-                  source
                 when Symbol
                   source.to_s
                 when Proc
