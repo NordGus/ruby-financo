0e62f54d56e8797a353af4117a5abc13fea43c9f
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-05 00:02:36 +0200

    redesigning deletion confirmation controls

--- app/javascript/controllers/deletable_controller.js
+++ app/javascript/controllers/deletable_controller.js
@@ -4,16 +4,18 @@ const DATA_HIDDEN_ATTRIBUTE = "data-hidden";
 
 // Connects to data-controller="deletable"
 export default class extends Controller {
-  static targets = ["confirmation", "trigger"];
+  static targets = ["confirmation", "trigger", "cancel"];
   connect() {}
 
   confirm(_event) {
     this.confirmationTarget.removeAttribute(DATA_HIDDEN_ATTRIBUTE);
+    this.cancelTarget.removeAttribute(DATA_HIDDEN_ATTRIBUTE);
     this.triggerTarget.setAttribute(DATA_HIDDEN_ATTRIBUTE, true);
   }
 
   cancel(_event) {
     this.confirmationTarget.setAttribute(DATA_HIDDEN_ATTRIBUTE, true);
+    this.cancelTarget.setAttribute(DATA_HIDDEN_ATTRIBUTE, true);
     this.triggerTarget.removeAttribute(DATA_HIDDEN_ATTRIBUTE);
   }
 }
