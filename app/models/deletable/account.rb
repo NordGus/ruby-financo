c92a1585017ee0a1064d578b70c6a970db577f16
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-07 00:46:25 +0200

    cleaning models

--- app/models/deletable/account.rb
+++ app/models/deletable/account.rb
@@ -9,12 +9,10 @@ module Deletable
     has_many :children, class_name: "Deletable::Account", foreign_key: "parent_id", dependent: :destroy
     has_many :debits,
              class_name: "Deletable::Transaction",
-             foreign_key: "source_id",
              dependent: :destroy,
              inverse_of: :source
     has_many :credits,
              class_name: "Deletable::Transaction",
-             foreign_key: "target_id",
              dependent: :destroy,
              inverse_of: :target
   end
