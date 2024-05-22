e1b4715caca1e1e4831e0afd477a4ccbb5f6e063
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2024-05-22 22:09:05 +0200

    setting up external_form.rb children

--- app/forms/accounts_and_goals/accounts/external_form.rb
+++ app/forms/accounts_and_goals/accounts/external_form.rb
@@ -3,6 +3,35 @@
 module AccountsAndGoals
   module Accounts
     class ExternalForm < AccountForm
+      attr_accessor :children
+
+      def initialize(account: nil, attributes: nil)
+        self.children = []
+        super
+      end
+
+      def children_attributes=(attributes)
+        attributes.each_value do |child_attributes|
+          @children << Child.new(child_attributes)
+        end
+      end
+
+      class Child
+        include ActiveModel::Model
+        include ActiveModel::Attributes
+
+        attribute :id, :string
+        attribute :name, :string
+        attribute :description, :string
+        attribute :archived_at, :datetime
+        attribute :archived, :boolean, default: false
+
+        attribute :capital, :integer, default: 0
+
+        validates :name,
+                  presence: true,
+                  length: { minimum: Account::NAME_MIN_LENGTH, maximum: Account::NAME_MAX_LENGTH }
+      end
     end
   end
 end
--- app/forms/accounts_and_goals/accounts/form_for.rb
+++ app/forms/accounts_and_goals/accounts/form_for.rb
@@ -8,7 +8,8 @@ module AccountsAndGoals
           "archived" => account.archived_at.present?,
           "at" => account.history&.debits&.first&.issued_at,
           "amount" => account.history&.debits&.first&.source_amount,
-          "personal_loan" => account.personal_loan?
+          "personal_loan" => account.personal_loan?,
+          "children_attributes" => map_account_children(account)
         }
 
         form_for(kind: account.kind, account:, attributes: attributes.compact)
@@ -22,18 +23,35 @@ module AccountsAndGoals
         form_for(kind: account.kind, account:, attributes:)
       end
 
-      def self.form_for(kind:, account: nil, attributes: {})
-        case kind
-        when *Account.capital_kinds_array
-          CapitalForm.new(account:, attributes:)
-        when *Account.debt_loan_kinds_array
-          Debt::LoanForm.new(account:, attributes:)
-        when *Account.debt_credit_line_kinds_array
-          Debt::CreditLineForm.new(account:, attributes:)
-        when *Account.external_kinds_array
-          ExternalForm.new(account:, attributes:)
-        else
-          raise ArgumentError, "unsupported kind: #{kind}"
+      class << self
+        private
+
+        def form_for(kind:, account: nil, attributes: {})
+          case kind
+          when *Account.capital_kinds_array
+            CapitalForm.new(account:, attributes:)
+          when *Account.debt_loan_kinds_array
+            Debt::LoanForm.new(account:, attributes:)
+          when *Account.debt_credit_line_kinds_array
+            Debt::CreditLineForm.new(account:, attributes:)
+          when *Account.external_kinds_array
+            ExternalForm.new(account:, attributes:)
+          else
+            raise ArgumentError, "unsupported kind: #{kind}"
+          end
+        end
+
+        def map_account_children(account)
+          return nil unless account.external?
+
+          Hash[account.children.map.with_index do |child, idx|
+            [
+              idx.to_s,
+              child.attributes
+                   .slice("id", "name", "description", "archived_at")
+                   .merge("archived" => child.archived_at.present?)
+            ]
+          end]
         end
       end
     end
